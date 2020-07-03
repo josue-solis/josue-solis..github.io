47931b500a0259142f0e85d367208b6b9b6603ea
Unikua <josue0509@gmail.com>
2020-07-02 20:27:43 -0600

    Menu Style and Responsive

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@ window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+var nav = document.querySelector(".navigation");
+var btnMenu = document.querySelector(".menu-btn");
+var btnClose = document.querySelector(".close-btn");
+
+btnMenu.addEventListener("click",function(){
+    nav.style.display = "flex";
+});
+
+btnClose.addEventListener("click",function(){
+    nav.style.display = "none";
+});
